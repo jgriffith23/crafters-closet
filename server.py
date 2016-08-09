4ce38b1166b14d97d1807a67c907dc88701e2bbe
jgriffith23 <jennifer.gd42@gmail.com>
2016-08-09 01:21:35 +0000

    Flask can display a homepage.

--- server.py
+++ server.py
@@ -1,30 +1,36 @@
-"""Fill in Flask server here!"""
-
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash, session, url_for
+from flask_debugtoolbar import DebugToolbarExtension
 
-from model import User, SupplyDetail, Project, ProjectSupplyDetail, Item
 from model import connect_to_db, db
 
 app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
 
-app.secret_key = "FIXME"
-
-# Have Jinja2 raise errors when variables are undefined
+# Normally, if you use an undefined variable in Jinja2, it fails
+# silently. This is horrible. Fix this so that, instead, it raises an
+# error.
 app.jinja_env.undefined = StrictUndefined
 
 
 @app.route("/")
-def homepage():
-    """Show the homepage."""
+def index():
+    """Homepage."""
 
     return render_template("homepage.html")
 
 
 if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
 
     connect_to_db(app)
-    app.run()
+
+    # Use the DebugToolbar
+    #DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
